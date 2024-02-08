3df4f3aec75564ded6cd61feaba056674f615ad2
Decembric <dmitro15121979@gmail.com>
2024-02-08 16:01:28 +0200

    add script

---
+++ src/js/1-timer.js
@@ -0,0 +1,30 @@
+const countdownDate = new Date('2025-01-01 00:00')
+const todayDate = new Date()
+const diff = countdownDate - todayDate
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(diff))
+
+function countdown () {
+  console.log('Interval')
+}
+
+setInterval(countdown, 1000)
